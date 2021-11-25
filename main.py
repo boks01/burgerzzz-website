ce5e396b75b9b70bd154a1d1bbb05bdf433b1db9
boks01 <msal40755@gmail.com>
2021-11-25 16:09:45 +0700

    os environ test

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "nasigoreng"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///cart_data.db"
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'nasigorengceplokpedas')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///cart_data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
